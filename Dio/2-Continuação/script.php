24b65821774b0a2f9a4465d91f6f8a214f8c2f63
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-17 16:40:03 -0300

    Criando formulário na WEB

---
+++ Dio/2-Continuação/script.php
@@ -0,0 +1,41 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'Infântil';
+$categorias[] = 'Adolescente';
+$categorias[] = 'Adulto';
+
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade <= 12) {
+    for($i = 0; $i <= count($categorias); $i++) {
+        if($categorias[i] == 'Infântil') {
+            echo "O nadador" .$nome. " compete na categoria Infântil.";
+        }
+
+    }
+
+} else if($idade >= 13 && $idade < 18) {
+    for($i = 0; $i <= count($categorias); $i++) {
+        if($categorias[i] == 'Adolescente') {
+            echo "O nadador" .$nome. " compete na categoria Adolescente.";
+        }
+
+    }
+
+} else {
+    for($i = 0; $i <= count($categorias); $i++) {
+        if($categorias[i] == 'Adulto') {
+            echo "O nadador" .$nome. " compete na categoria Adulto.";
+        }
+
+    }
+
+}
+    
