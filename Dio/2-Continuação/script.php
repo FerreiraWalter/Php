2c3c73f32e28ee87639981301055e94c5f784745
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-17 16:49:51 -0300

    Conserto de erros

--- Dio/2-Continuação/script.php
+++ Dio/2-Continuação/script.php
@@ -14,28 +14,12 @@ $idade = $_POST['idade'];
 //var_dump($idade);
 
 if($idade <= 12) {
-    for($i = 0; $i <= count($categorias); $i++) {
-        if($categorias[i] == 'Infântil') {
-            echo "O nadador" .$nome. " compete na categoria Infântil.";
-        }
-
-    }
+    echo "O nadador " .$nome. " compete na categoria Infântil.";
 
 } else if($idade >= 13 && $idade < 18) {
-    for($i = 0; $i <= count($categorias); $i++) {
-        if($categorias[i] == 'Adolescente') {
-            echo "O nadador" .$nome. " compete na categoria Adolescente.";
-        }
-
-    }
+    echo "O nadador " .$nome. " compete na categoria Adolescente.";
 
 } else {
-    for($i = 0; $i <= count($categorias); $i++) {
-        if($categorias[i] == 'Adulto') {
-            echo "O nadador" .$nome. " compete na categoria Adulto.";
-        }
-
-    }
-
+    echo "O nadador " .$nome. " compete na categoria Adulto.";
 }
     
