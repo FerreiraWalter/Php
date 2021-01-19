134a8b5ed0dda7a87bfbc4fd501f151c6d524cfc
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-19 19:43:02 -0300

    Desafio Switch

---
+++ Cod3r/Operadores/Desafio_Switch.php
@@ -0,0 +1,29 @@
+<div>DESAFIO SWITCH</div>
+
+<form action="#" method="post">
+
+    <input type="text" name="param">
+    <select name="conversao" id="conversao">
+        <option value="km-milha">Km -> Milha</option>
+        <option value="milha-km">Milha -> Km</option>
+        <option value="metro-km">Metro -> Km</option>
+        <option value="km-metro">Km -> Metro</option>
+    </select>
+
+    <button>Calcular</button>
+
+</form>
+
+<style>
+
+    form > * {
+        font-size: 1.8rem;
+    }
+
+</style>
+
+<?php
+
+
+
+?>
