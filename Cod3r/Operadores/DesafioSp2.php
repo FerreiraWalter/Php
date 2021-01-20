4a4d62c7ac86b183036d35b1055402cd31407993
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-20 10:26:33 -0300

    Desafio Switch Parte 2

---
+++ Cod3r/Operadores/DesafioSp2.php
@@ -0,0 +1,89 @@
+<div class="titulo">Desafio Switch</div>
+
+<form action="#" method="post">
+    <input type="text" name="param">
+    <select name="conversao" id="conversao">
+        <option value="km-milha">Km > Milha</option>
+        <option value="milha-km">Milha > Km</option>
+        <option value="metro-km">Metros > Km</option>
+        <option value="km-metro">Km > Metros</option>
+        <option value="cel-fah">Celsius > Fahrenheit</option>
+        <option value="fah-cel">Fahrenheit > Celsius</option>
+    </select>
+    <button>Calcular</button>
+</form>
+
+<style>
+    form > * {
+        font-size: 1.8rem;
+    }
+</style>
+
+<?php
+
+const FATOR_KM_MILHA = 0.621371;
+const FATOR_METRO_KM = 1000;
+const FATOR_CEL_FAH = 1.8;
+
+$param = $_POST['param'] ?? 0;
+
+switch ($_POST['conversao']) {
+    case 'km-milha':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+        
+        $distancia = $param * FATOR_KM_MILHA;
+        $mensagem = "$param Km(s) = $distancia Milha(s)";
+        break;
+
+    case 'milha-km':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+
+        $distancia = $param / FATOR_KM_MILHA;
+        $mensagem = "$param Milha(s) = $distancia Km(s)";
+        break;
+
+    case 'metro-km':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+
+        $distancia = $param / FATOR_METRO_KM;
+        $mensagem = "$param Metro(s) = $distancia Km(s)";
+        break;
+
+    case 'km-metro':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+
+        $distancia = $param * FATOR_METRO_KM;
+        $mensagem = "$param Km(s) = $distancia Metro(s)";
+        break;
+
+    case 'cel-fah':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+
+        $conversao = $param * FATOR_CEL_FAH + 32;
+        $mensagem = "{$param}° Celsius = {$conversao}° Fahrenheit";
+        break;
+
+    case 'fah-cel':
+        if(!is_numeric($param)) {
+            $param = 0;
+        }
+
+        $conversao = ($param - 32) / FATOR_CEL_FAH;
+        $mensagem = "{$param}° Fahrenheit = {$conversao}° Celsius";
+        break;
+
+    default:
+        $mensagem = "Nenhum valor calculado!";
+}
+
+echo "<p>$mensagem</p>";
