a1165f4587bdb7755997dbe77d8d6ab66e0dc7ac
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-29 15:38:56 -0300

    Sistema de Cadastro funcionando

--- ProjetoSite/cadastrar.php
+++ ProjetoSite/cadastrar.php
@@ -1,3 +1,8 @@
+<?php
+    require_once 'usuarios.php';
+    $u = new Usuario; 
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,7 +14,7 @@
 <body>
     <div id="corpo-form">
         <h1>Cadastrar</h1>
-        <form action="processa.php" method="post">
+        <form method="POST">
             <input type="text" name="nome" placeholder="Nome Completo" maxlenght="30">
             <input type="text" name="telefone" placeholder="Telefone" maxlenght="30">
             <input type="email" name="email" placeholder="Usuário" maxlenght="40">
@@ -21,5 +26,40 @@
             <a href="central.php">Já tem uma conta? <strong>Clique aqui</strong></a>
         </form>
     </div>
+
 </body>
 </html>
+
+<?php
+    //verificar se clicou no botão
+    if(isset($_POST['nome'])) {
+        $nome = addslashes($_POST['nome']);
+        $telefone = addslashes($_POST['telefone']);
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+        $confirmarSenha = addslashes($_POST['Confsenha']);
+
+        if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) {
+            $u->conectar("projeto_login", "localhost", "root", "");
+            
+            if($u->msgErro == "") {
+                if($senha == $confirmarSenha){
+                    if ($u->cadastrar($nome, $telefone, $email, $senha)) {
+                        echo "Cadastrado com sucesso!";
+                    } else {
+                        echo "Email já cadastrado";
+                    }
+                } else {
+                    echo "Senhas não correspondem!";
+                }
+            } else {
+                echo "Erro:" .$u->msgErro;
+            }
+            
+        } else {
+            echo "Preencha todos os campos!";
+        }
+
+    }
+
+?>
---
+++ ProjetoSite/usuarios.php
@@ -0,0 +1,55 @@
+<?php
+
+Class Usuario {
+
+    private $pdo;
+    public $msgErro = ""; //Ok
+
+    public function conectar($nome, $host, $usuario, $senha) {
+        global $pdo;
+        try {
+            $pdo = new PDO("mysql:dbname=".$nome.";host=".$host, $usuario, $senha);
+        } catch (PDOException $e) {
+            $msgErro = $e->getMessage();
+        }
+    }
+
+    public function cadastrar($nome, $telefone, $email, $senha) {
+        global $pdo;
+        //Verificar se já está cadastrado
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+
+        $sql->bindValue(":e", $email);
+        $sql->execute();
+
+        if($sql->rowCount() > 0) {
+            return false; // já esta cadastrada
+        }
+        else {
+            //Caso não, cadastrar
+            $sql = $pdo->prepare("INSERT INTO usuarios (nome, telefone, email, senha) VALUES ('{$nome}', '{$telefone}', '{$email}', '{$senha}')");
+            $sql->execute();
+            return true; //Tudo Ok
+        }
+    }
+
+    public function logar($email, $senha) {
+        global $pdo;
+        //Verificar se o email e senha estão cadastrados
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha = :s");
+        $sql->bindValue(":e", $email);
+        $sql->bindValue(":s", md5($senha));
+
+        if($sql->rowCount() > 0) {
+            $dado = $sql->fetch(key);
+            session_start();
+
+            $SESSION['id_usuario'] = $dado['id_usuario'];
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
