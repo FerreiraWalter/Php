befd42f68ee6883e5a35fe1db4d003ce98c18f16
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-01 16:10:17 -0300

    Conserto

---
+++ Use_A_Cabeca/indexx.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send a Message</title>
+
+    <style>
+    
+        body {
+            background: url("space.jpg");
+        }
+
+    </style>
+
+</head>
+<body>
+    
+    <div>
+        <form action="" method="GET">
+            
+        </form>
+    </div>
+
+</body>
+</html>
