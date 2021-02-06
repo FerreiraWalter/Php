83b734d48d0b6429ca92282605537d40a41d02f5
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-06 00:05:36 -0300

    Enviar email

--- Site_Elvis/enviarEmail.php
+++ Site_Elvis/enviarEmail.php
@@ -1,3 +1,24 @@
 <?php
     include("cabecalho.php");
 ?>
+
+<formaction="#" method="POST">
+    <h2>Enviar Email</h2>
+    <input type="email" placeholder = "Your Email">
+    <br>
+    <textarea class="funcionapls" name="texto" id="" cols="40" rows="20"></textarea>
+    <br>
+    <button>Enviar</button>
+</form>
+
+<a href="indexx.php"><p>Voltar para o Menu Principal</p></a>
+
+<?php
+
+    if(isset($_POST['texto'])) {
+        $from = 'wferreiraramosjunior@gmail.com';
+        $objetivo = $_POST['texto'];
+
+        $query = "SELECT * FROM cadastros";
+
+    }
