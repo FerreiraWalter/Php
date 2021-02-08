6b85e96c00b0dc1b4331d8afb7d82555c6f0d44f
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-08 10:13:03 -0300

    ajeitando paginas

--- Site_Elvis/enviarEmail.php
+++ Site_Elvis/enviarEmail.php
@@ -4,7 +4,7 @@
 
 <formaction="#" method="POST">
     <h2>Enviar Email</h2>
-    <input type="email" placeholder = "Your Email">
+    <input type="email" name="email" placeholder = "Your Email">
     <br>
     <textarea class="funcionapls" name="texto" id="" cols="40" rows="20"></textarea>
     <br>
@@ -15,12 +15,17 @@
 
 <?php
 
-    if(isset($_POST['texto'])) {
+
         $from = 'wferreiraramosjunior@gmail.com';
         $objetivo = $_POST['texto'];
-
-        $query = "SELECT * FROM cadastros";
-
-    }
-
     
+        $conexao = mysqli_connect("localhost", "root", "", "makemeelvis");
+    
+        $query = "SELECT * FROM cadastros";
+        $resultado = mysqli_query($conexao, $query);
+    
+        $row = mysqli_fetch_array($resultado);
+        echo $row['prinome'] . ' ' . $row['email'];
+        $row = mysqli_fetch_array($resultado);
+        echo $row['prinome'] . ' ' . $row['email'];
+
