24e564bcb23d718782b22b61077b7eb2550ba1d5
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-08 16:40:50 -0300

    Sistema de Excluir cadastro quase finalizado

--- Site_Elvis/removeremail.php
+++ Site_Elvis/removeremail.php
@@ -1,3 +1,37 @@
 <?php 
     include("cabecalho.php");
+?>
+
+<form action="#" method="POST">
+    <h2>Excluir Email</h2>
+    <input type="email" name="email" placeholder="Email">
+    <br>
+    <button>Excluir</button>
+    <button name="clique">Clientes</button>
+
+</form>
+
+<a href="indexx.php"><p>Voltar para o Menu Principal</p></a>
+
+<?php
+
+    if(isset($_POST['email'])) {
+        $email = $_POST['email'];
+        $conexao = mysqli_connect("localhost", "root", "", "makemeelvis");
+
+        $query_lista = "SELECT * FROM cadastros";
+        $resultado_lista = mysqli_query($conexao, $query_lista);
     
+        while($row = mysqli_fetch_array($resultado_lista)) {
+            echo $row['prinome'] . " " . $row['ultnome'] . " - ". $row['email'] . "<br />";
+        }
+
+        if(empty($email)) {
+            echo "Campo vazio, não foi Excluido nenhum email";
+        } else {
+            $query_delete = "DELETE FROM cadastros WHERE email = '{$email}'";
+            $resultado_delete = mysqli_query($conexao, $query_delete);
+        }
+
+    }
+
